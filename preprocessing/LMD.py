2c876c2db8e3778fe0cc450b018f49f2e0b0bde8
Bryan Gotti <bry.gotti@outlook.com>
2025-05-15 15:57:23 +0200

    Preprocessing

---
+++ preprocessing/LMD.py
@@ -0,0 +1,44 @@
+import numpy as np
+from PyLMD import LMD
+
+lmd = LMD()
+
+def lmd_features(x: np.ndarray) -> np.ndarray:
+    """
+    Implement feature extraction of the following paper:
+    https://www.sciencedirect.com/science/article/pii/S0010482520302614#bib18 (Scalp EEG classification using deep Bi-LSTM network for seizure detection)
+    """
+    x = x.T
+    channels = []
+    for i in range(x.shape[0]):
+        # Apply LMD to each channel
+        PFs, _ = lmd.lmd(x[i])
+        channels.append(PFs[:3]) # Take the first 3 PFs
+    channels = np.stack(channels)
+    return extract_features(channels)
+
+
+def extract_features(channels: np.ndarray) -> np.ndarray:
+    """
+    Get the features from the PFs
+    Args:
+        PFs: The PFs
+    Returns:
+        The features
+    """
+    return np.concatenate([
+        np.max(channels, axis=-1),
+        np.min(channels, axis=-1),
+        np.median(channels, axis=-1),
+        np.mean(channels, axis=-1),
+        np.std(channels, axis=-1),
+        np.var(channels, axis=-1),
+        # Mean absolute deviation
+        np.mean(np.abs(channels - np.mean(channels, axis=-1, keepdims=True)), axis=-1),
+        # Root mean square
+        np.sqrt(np.mean(channels**2, axis=-1)),
+        # Skewness
+        np.mean((channels - np.mean(channels, axis=-1, keepdims=True))**3, axis=-1) / (np.std(channels, axis=-1)**3),
+        # Kurtosis
+        np.mean((channels - np.mean(channels, axis=-1, keepdims=True))**4, axis=-1) / (np.std(channels, axis=-1)**4),
+    ], axis=1)
