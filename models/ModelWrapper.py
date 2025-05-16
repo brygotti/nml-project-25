11d0b9230d08188334833b42ef7b09542423fc9e
Bryan Gotti <bry.gotti@outlook.com>
2025-05-16 11:46:31 +0200

    Add model to ModelWrapper

--- models/ModelWrapper.py
+++ models/ModelWrapper.py
@@ -1,12 +1,14 @@
 
 from models.SimpleLSTM import SimpleLSTM
-
+from models.TemporalBiLSTM import TemporalBiLSTM
 
 
 # Add your model here
 def get_model(model_name='SimpleLSTM', model_params=None, device=None):
     if model_name == 'SimpleLSTM':
         return SimpleLSTM(**model_params).to(device)
+    elif model_name == 'TemporalBiLSTM':
+        return TemporalBiLSTM(**model_params).to(device)
     else:
         raise ValueError(f"Unsupported model: {model_name}")
 
