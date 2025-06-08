434afb2c40fad1624bf2306843253e652cd31ef0
Bryan Gotti <bry.gotti@outlook.com>
2025-06-08 18:10:59 +0200

    Add README and env.yml, cleanup

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# EE-452 - Graph-based EEG Analysis
+# Pipeline for the project EE-452 - Graph-based EEG Analysis
+
+## Requirements
+To run the pipeline, please install the conda environment from the `env.yml` file:
+
+```bash
+conda env create -f env.yml
+conda activate nml
+```
+
+## Running the pipeline
+You can edit the `main.py`, it contains a configuration object that allows you to set the parameters for the pipeline. You can choose the model, preprocessing, caching, hyperparameters, and more.
+Make sure to adjust the paths to your data and results directories as needed.
+To run the pipeline, execute the following command:
+
+```bash
+python main.py
+```
+
+This will start the training and evaluation process based on the configuration provided in `main.py`. It will then generate the submission file for the Kaggle competition.
--- main.py
+++ main.py
@@ -20,7 +20,7 @@ print(f"Positive weight for loss function: {pos_weight}")
 
 ## Add you config file here 
 CONFIG = {
-    "k_folds": 3, # Number of folds for K-fold cross validation
+    "k_folds": 5, # Number of folds for K-fold cross validation
     "data_path": "data",
     "batch_size": 512,
     "num_epochs": 100,
@@ -28,23 +28,19 @@ CONFIG = {
     "lr": 1e-4,
     "model": "GCNN",
     "model_params": {
-        "conv_channels": (10, 64, 32),
+        "conv_channels": (354, 64, 32),
     },
     "criterion_fn": lambda: nn.BCEWithLogitsLoss(), #pos_weight=torch.tensor(pos_weight)),
-    # "cached_preprocessing": {
-    #     "train": "tmp/lmd_train_arrays.npy",
-    #     "test": "tmp/lmd_test_arrays.npy"
-    # },
-    "signal_transform": lambda x: frequency_bands_features(x, flatten=False),
+    "signal_transform": lambda x: fft_filtering(x, transpose=True),
     "early_stopping": {
         "metric": "f1_score",        # Metric to monitor for early stopping
         "greater_is_better": True,   # Whether a higher value of the metric is better
         "validation_size": 0.2,      # Size of validation set for early stopping
         "max_epochs": 10000,         # Maximum epochs to train before early stopping
         "patience": 10,              # Number of epochs with no improvement after which training will be stopped
-        "delta_tolerance": 0.1       # Deltas in F1 score higher than that will be considered as "no improvement" and trigger early stopping
+        "delta_tolerance": 0.05      # Deltas in F1 score higher than that will be considered as "no improvement" and trigger early stopping
     },
-    "graph_cache_name": "distances_frequency_bands_features",
+    "graph_cache_name": "distances_fft_filtering",
     "generate_graph": lambda distances, nodes_order, x: generate_distances_graph(distances, nodes_order), # Set this if you want to work with graphs
     # the generate_graph function should return a Data object with edge_index defined. The signal_transform output should also have 2 dimensions: (n_nodes, n_features)
 }
