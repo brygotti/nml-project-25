6f8d35e33289b931de8df2b3c7b5a2918b3ef229
Iris Olympe Segard <isegard@izar1.izar.cluster>
2025-06-10 09:59:00 +0200

    Conformer

--- models/ModelWrapper.py
+++ models/ModelWrapper.py
@@ -2,6 +2,7 @@ from models.SimpleLSTM import SimpleLSTM
 from models.TemporalBiLSTM import TemporalBiLSTM
 from models.TemporalLSTMCombinedChannels import TemporalLSTMCombinedChannels
 from models.GCNN import GCNN
+from models.Conformer import Conformer
 from torchinfo import summary
 
 
@@ -17,6 +18,8 @@ def get_model(model_name='SimpleLSTM', model_params=None, device=None):
         model = TemporalLSTMCombinedChannels(**model_params)
     elif model_name == 'GCNN':
         model = GCNN(**model_params)
+    elif model_name == 'Conformer': 
+        model = Conformer(**model_params)
     else:
         raise ValueError(f"Unsupported model: {model_name}")
     
