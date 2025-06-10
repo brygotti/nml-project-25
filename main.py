c1fabc3d5ccda0f871b48ffea9052df585c90391
loic <misentaloic@gmail.com>
2025-06-10 16:00:27 +0200

    lomimi correction

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from preprocessing.FFTTransform import fft_filtering
 from training_pipeline import *
 from utils import *
 from preprocessing.Graphs import generate_distances_graph
+from preprocessing.Graphs import build_graph_dataset, generate_single_graph
 
 ## Edit this config to change the training parameters
 CONFIG = {
@@ -14,20 +15,18 @@ CONFIG = {
     "batch_size": 512, # Can be set to "session" to use each recording session as a batch
     "num_epochs": 100, # Ignored if early stopping is enabled
     "optimizer": None, # Defaults to Adam with learning rate config["lr"]
-    "lr": 1e-4,
-    "model": "GCNN",
+    "lr": 1e-3,
+    "model": "EEGNet",
     "criterion_fn": lambda weight_pos_class: nn.BCEWithLogitsLoss(), # weight_pos_class can be used in the loss function to reweight the loss for the positive class
+    "signal_transform": fft_filtering,
     "early_stopping": {
         "metric": "f1_score",        # Metric to monitor for early stopping
         "greater_is_better": True,   # Whether a higher value of the metric is better
         "validation_size": 0.2,      # Size of validation set for early stopping
         "max_epochs": 10000,         # Maximum epochs to train before early stopping
-        "patience": 10,              # Number of epochs with no improvement after which training will be stopped
+        "patience": 20,              # Number of epochs with no improvement after which training will be stopped
         "delta_tolerance": 0.05      # Deltas in F1 score higher than that will be considered as "no improvement" and trigger early stopping
-    },
-    "graph_cache_name": "distances_fft_filtering",
-    "generate_graph": lambda distances, nodes_order, x: generate_distances_graph(distances, nodes_order, fft_filtering(x, transpose=True)), # Set this if you want to work with graphs
-    # The generate_graph function should return a Data object with edge_index and x defined.
+    }
 }
 
 if __name__ == "__main__":
